822836281248dbb1e100aa6f58573cfe94c4a53c
Aaron <aaron.remarchuk@utp.ac.pa>
2025-06-16 10:07:22 -0500

    indicaciones

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -1,10 +1,29 @@
 //Simulador de Navegacion en un Navegador Web
+
+//utilizar una pila, implementando: INSERTAR(push), ELIMINAR(pop), CIMA y PilaVacia
+
+
 //se requiere implementar un sistema de navegacion basica:
 //  visitar visitar una nueva pagina (push)
 //  atras   regresar a la pagina anterior (pop)
 //  actual  muestra la pagina web acutal (cima)
 //  mostrar_historial   muestra todas las paginas en la pila (de arriba hacia abajo)
 
+//Ejemplo de uso
+//visitar("https://..1")
+//visitar("https://..2")
+//visitar("https://..3")
+// actual()
+// https://..3
+// atras()
+// actual()
+// https://..2
+// mostrar_historial()
+// https://..1
+// https://..2
+
+//Entregar en formato .txt
+// con un archivo README con instruccioness de uso y resumen de lo aprendido
 
 public class Main {
     public static void main(String[] args) {
