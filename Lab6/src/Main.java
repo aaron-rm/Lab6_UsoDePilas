a0dc14e0a726950ad9e12294e878d93758b9d736
Aaron <aaron.remarchuk@utp.ac.pa>
2025-06-16 10:02:09 -0500

    2

---
+++ Lab6/src/Main.java
@@ -0,0 +1,13 @@
+//Simulador de Navegacion en un Navegador Web
+//se requiere implementar un sistema de navegacion basica:
+//  visitar visitar una nueva pagina (push)
+//  atras   regresar a la pagina anterior (pop)
+//  actual  muestra la pagina web acutal (cima)
+//  mostrar_historial   muestra todas las paginas en la pila (de arriba hacia abajo)
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
